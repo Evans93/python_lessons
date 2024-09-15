5b6de1c5ca99fa9f3ef65e4c8965aa62130b7bb1
Evans93 <evansehis7837@gmail.com>
2024-09-15 21:22:31 +0200

    testing

---
+++ ola.py
@@ -0,0 +1,12 @@
+def generic_arithmetic(first_name , last_name):
+    x = first_name
+
+    y = last_name
+
+    return x + y
+
+
+print(generic_arithmetic("Ehis"," Evans"))
+
+
+print(generic_arithmetic("Akinseoye", " Olajumoke"))
